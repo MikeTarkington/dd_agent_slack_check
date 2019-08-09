abab2ddf8fee8e4a0743b4225cf46fa2e9a27505
Mike Tarkington <tarkmike@gmail.com>
2019-08-08 17:03:01 -0700

    Add support for submitting metrics re multiple channels and use of yaml config values

--- slack_data.py
+++ slack_data.py
@@ -16,40 +16,41 @@ import time
 
 class SlackData(AgentCheck):
     def check(self, instance):
-        url = 'https://slack.com/api/conversations.history'
-        data = [
-            ('token', '<token_string>'),
-            # refactor to run on all channels set in config yaml file
-            ('channel', 'C8VH6NR2A'),
-            ('oldest', time.time() - 86400)
-        ]
-        r = requests.get(url, data)
-        chan_history = json.loads(r.text)
+        for channel in instance['channels']:
+            url = 'https://slack.com/api/conversations.history'
+            data = [
+                ('token', instance['slack_app_token']),
+                ('channel', channel),
+                ('oldest', time.time() - 86400)
+            ]
+            r = requests.get(url, data)
+            chan_history = json.loads(r.text)
 
-        unanswered = 0
-        response_times = []
-        avg_response_time_24hr = None
-        for message in chan_history["messages"]:
-            if "reply_count" not in message.keys() and "subtype" not in message.keys():
-                unanswered += 1
-            elif "reply_count" in message.keys() and "subtype" not in message.keys():
-                self_answer = 0
-                reply_time = 0
-                for reply in reversed(message["replies"]):
-                    if reply["user"] == message["user"]:
-                        self_answer += 1
-                    elif reply["user"] != message["user"]:
-                        reply_time = (
-                            float(reply["ts"]) - float(message["ts"])) / 60
-                        response_times.append(reply_time)
-                        break
-                if self_answer == message["reply_count"]:
+            unanswered = 0
+            response_times = []
+            avg_response_time_24hr = None
+            for message in chan_history["messages"]:
+                if "reply_count" not in message.keys() and "subtype" not in message.keys():
                     unanswered += 1
+                elif "reply_count" in message.keys() and "subtype" not in message.keys():
+                    self_answer = 0
+                    reply_time = 0
+                    for reply in reversed(message["replies"]):
+                        if reply["user"] == message["user"]:
+                            self_answer += 1
+                        elif reply["user"] != message["user"]:
+                            reply_time = (
+                                float(reply["ts"]) - float(message["ts"])) / 60
+                            response_times.append(reply_time)
+                            break
+                    if self_answer == message["reply_count"]:
+                        unanswered += 1
 
-        if response_times:
-            avg_response_time_24hr = sum(response_times) / len(response_times)
+            if response_times:
+                avg_response_time_24hr = sum(
+                    response_times) / len(response_times)
 
-        self.gauge('slack.data.unanswered', unanswered, tags=[
-                   'SLACK_CHECK_TEST:test', "channel:general"])
-        self.gauge('slack.data.24hr_avg_response_time',
-                   avg_response_time_24hr, tags=['channel:general'])
+            self.gauge('slack.data.unanswered', unanswered,
+                       tags=['channel:{}'.format(channel)])
+            self.gauge('slack.data.24hr_avg_response_time', avg_response_time_24hr, tags=[
+                       'channel:{}'.format(channel)])
